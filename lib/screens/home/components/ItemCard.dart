366608b47b69316af874c6d11d1de2d00d6e0faa
Fatih Kayan <fatih.kayan83@gmail.com>
2020-09-24 18:34:52 +0300

    Completed

--- lib/screens/home/components/ItemCard.dart
+++ lib/screens/home/components/ItemCard.dart
@@ -25,7 +25,10 @@ class ItemCard extends StatelessWidget {
               color: product.color,
               borderRadius: BorderRadius.circular(16),
             ),
-            child: Image.asset(product.image),
+            child: Hero(
+              tag: product.id,
+              child: Image.asset(product.image),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
